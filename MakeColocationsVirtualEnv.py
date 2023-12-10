21a4c8f6beaa95018943f46e5364e60a514c6e89
wndrsn1 <146864301+wndrsn1@users.noreply.github.com>
2023-12-09 20:16:51 -0600

    MakeColocationsVirtualEnv.py

--- MakeColocationsVirtualEnv.py
+++ MakeColocationsVirtualEnv.py
@@ -6,12 +6,9 @@ import sys
 conda_env_name = "ColocationsEnv"
 
 def main():
-    # List of libraries to install
     conda_libraries = ['pandas','numpy','h5py','xarray','shapely','geopandas','dask','tqdm']  # Add more libraries as needed
-    #pip_libraries = ['modis-tools']
-
     conda_install(conda_libraries)
-    #pip_install(pip_libraries)
+
 
 def conda_install(conda_libraries):
     commands = [
@@ -23,9 +20,6 @@ def conda_install(conda_libraries):
     os.system(f'conda create -n {conda_env_name} --yes')
     for command in commands:
         os.system(f'conda run -n {conda_env_name} {command}')
-
-def pip_install(pip_libraries):
-    subprocess.run([sys.executable, '-m', 'pip', 'install'] + pip_libraries)
 
 
 main()
