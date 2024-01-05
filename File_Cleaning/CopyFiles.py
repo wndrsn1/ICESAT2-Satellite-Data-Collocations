d1e38aec827cd0118aab6a55937fe887397d59bf
Will Anderson <146864301+wndrsn1@users.noreply.github.com>
2024-01-05 11:59:22 -0600

    Update CopyFiles.py

--- File_Cleaning/CopyFiles.py
+++ File_Cleaning/CopyFiles.py
@@ -20,10 +20,9 @@ def duplicate_h5_and_hdf_files(source_folder, destination_folder, file_patterns)
                     # Copy the H5 file
                     shutil.copy(source_file, destination_file)
 
-                    # Get the current date for logging
-                    current_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-                    print(f"{current_date}: File '{source_file}' duplicated successfully as '{destination_file}'.")
-
+                    # # Get the current date for logging
+                    # current_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                    print(f"File {source_file} duplicated successfully as {destination_file}.")
                 except Exception as e:
                     print(f"Error: {e}")
 
@@ -32,14 +31,17 @@ def duplicate_h5_and_hdf_files(source_folder, destination_folder, file_patterns)
 
 # Example usage
 source_folder_path = "/nfsscratch/Users/wndrsn"
-destination_folder_path = "/nfsscratch/Users/wndrsn/ColocationFiles"
-test = pd.read_csv('/Users/wndrsn/colocationsListFinal.csv', usecols=['filename_right', 'filename_left'])
+filename = 'Colocation_Files'
+destination_folder_path = f"/nfsscratch/Users/wndrsn/{filename}"
+if os.path.exists(destination_folder_path) == False:
+    os.mkdir(destination_folder_path) 
+test = pd.read_csv(os.path.join('/Users/wndrsn',(filename +'.csv')), usecols=['filename_right', 'filename_left'])
 test = test.drop_duplicates(subset=['filename_left'], keep='first', inplace=False, ignore_index=False)
 test = test.drop_duplicates(subset=['filename_right'], keep='first', inplace=False, ignore_index=False)
 print('Creating Colocation_Files.csv.. ')
-test.to_csv('/Users/wndrsn/Colocation_Files.csv')
+test.to_csv('/Users/wndrsn/Colocation_Files2020.csv')
 print('Created Colocation_Files.csv!')
-# Extract unique values from the DataFrame columns as file patterns
+
 file_patterns = test['filename_right'].tolist() + test['filename_left'].tolist()
 
 duplicate_h5_and_hdf_files(source_folder_path, destination_folder_path, file_patterns)
