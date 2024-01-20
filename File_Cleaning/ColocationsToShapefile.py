812f08fcf8cc258b3fd9428f9d34afdeed2ecd21
Will Anderson <146864301+wndrsn1@users.noreply.github.com>
2024-01-20 15:39:37 -0600

    Update ColocationsToShapefile.py

--- File_Cleaning/ColocationsToShapefile.py
+++ File_Cleaning/ColocationsToShapefile.py
@@ -3,36 +3,28 @@ import geopandas as gpd
 from shapely.geometry import Point
 from tqdm import tqdm
 import os
-import glob
 
-os.chdir('/Users/wndrsn/')
-path = '/nfsscratch/Users/wndrsn/2019_Shapefiles'
-search_text = '2019'
-filetype = '.csv'
-files = glob.glob(os.path.join(path, '**/*{filetype}'), recursive=True)
-files = [file for file in files if search_text in file.lower() and file.endswith(filetype)]
-for filepath in tqdm(files):
-    # Extract the file name without extension
-    file_name = os.path.splitext(os.path.basename(filepath))[0]
+filenames = ['colocationsListFinal2019','colocationsListFinal2020','colocationsListFinal2021']
 
-    # Output shapefile paths
-    output_shapefile_left = f'ATL_{file_name}.shp'  # Add an underscore for better naming
-    output_shapefile_right = f'MODIS_{file_name}.shp'  # Add an underscore for better naming
+# Initialize an empty list to store GeoDataFrames
+gdf_list = []
 
-    # Read the entire CSV
-    data = pd.read_csv(filepath, usecols=['Long0_right', 'Lat0_right', 'Long0_left', 'Lat0_left'])
-    print('CSV file read!')
-
-    # Process the right side
-    geometry_right = [Point(xy) for xy in zip(data['Long0_right'], data['Lat0_right'])]
-    gdf_right = gpd.GeoDataFrame(data, geometry=geometry_right, crs='EPSG:4326')
-    gdf_right.to_file(output_shapefile_right, mode='w', driver='ESRI Shapefile')
-    print('Created MODIS shapefile')
-
+for filename in filenames:
+    output_shapefile_left = f'ATL_{filename}.shp'  # Add an underscore for better naming
+    test = pd.read_csv(os.path.join('/Users/wndrsn', (filename + '.csv')),
+                       usecols=['filename_right', 'filename_left', 'Long0_left', 'Lat0_left'])
+    test = test.drop_duplicates(subset=['filename_left'], keep='first', inplace=False, ignore_index=False)
+    test = test.drop_duplicates(subset=['filename_right'], keep='first', inplace=False, ignore_index=False)
+    
     # Process the left side
-    geometry_left = [Point(xy) for xy in zip(data['Long0_left'], data['Lat0_left'])]
-    gdf_left = gpd.GeoDataFrame(data, geometry=geometry_left, crs='EPSG:4326')
-    gdf_left.to_file(output_shapefile_left, mode='w', driver='ESRI Shapefile')
+    geometry_left = [Point(xy) for xy in zip(test['Long0_left'], test['Lat0_left'])]
+    gdf_left2 = gpd.GeoDataFrame(test, geometry=geometry_left, crs='EPSG:4326')
 
-    print(f'Shapefiles saved at: {output_shapefile_left}, {output_shapefile_right}')
-    break
+    # Append gdf_left2 to the list
+    gdf_list.append(gdf_left2)
+
+# Concatenate all GeoDataFrames in the list
+gdf_left = gpd.GeoDataFrame(pd.concat(gdf_list, ignore_index=True), crs='EPSG:4326')
+
+# Save the final GeoDataFrame to a shapefile
+gdf_left.to_file(output_shapefile_left, mode='w', driver='ESRI Shapefile')
