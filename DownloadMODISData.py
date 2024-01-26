d4ece0bbaaa4e769c35154f18f6e2a99f1ae84c1
Will Anderson <146864301+wndrsn1@users.noreply.github.com>
2024-01-26 11:21:17 -0600

    Update DownloadMODISData.py

--- DownloadMODISData.py
+++ DownloadMODISData.py
@@ -4,11 +4,12 @@ os.chdir('/nfsscratch/Users/wndrsn')
 access_token = ''
 
 
-def wipe_directory():    
-    command_1 ="rm -rf /nfsscratch/Users/wndrsn/MOD06_L2"
-    os.system(command_1)
-    print('Removed MOD06_L2 directory')
+def Download_data():    
     days = ['00' + str(_) for _ in range(1,10)] + ['0' + str(_) for _ in range(10,100)] + [str(_) for _ in range(100,366)]
-    for day in days:
-        command_2 = f'wget -e robots=off -m -np -R .html,.tmp -nH --cut-dirs=3 "https://ladsweb.modaps.eosdis.nasa.gov/archive/allData/61/MOD06_L2/2019/{day}/" --header "Authorization: Bearer {access_token}" -P .'
-        os.system(command_2)
+    years = ['2019','2020','2021','2022','2023']
+    for year in years:
+        for day in days:
+            command_1 = f'wget -e robots=off -m -np -R .html,.tmp -nH --cut-dirs=3 "https://ladsweb.modaps.eosdis.nasa.gov/archive/allData/61/MOD06_L2/{year}/{day}/" --header "Authorization: Bearer {access_token}" -P .'
+            command_2 = f'wget -e robots=off -m -np -R .html,.tmp -nH --cut-dirs=3 "https://ladsweb.modaps.eosdis.nasa.gov/archive/allData/61/MYD06_L2/{year}/{day}/" --header "Authorization: Bearer {access_token}" -P .'
+            os.system(command_1)
+            os.system(command_2)
