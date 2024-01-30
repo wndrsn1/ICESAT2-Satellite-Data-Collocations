f2d4cffa6a1aad2afbd897b071544154e7a1a2ab
Will Anderson <146864301+wndrsn1@users.noreply.github.com>
2024-01-30 14:03:13 -0600

    Update ColocationsToShapefile.py

--- File_Cleaning/ColocationsToShapefile.py
+++ File_Cleaning/ColocationsToShapefile.py
@@ -5,27 +5,34 @@ from tqdm import tqdm
 import os
 
 filenames = ['colocationsListFinal2019','colocationsListFinal2020','colocationsListFinal2021']
+def create_shapefile(columnsList,side):
+    # Initialize an empty list to store GeoDataFrames
+    gdf_list = []
 
-# Initialize an empty list to store GeoDataFrames
-gdf_list = []
+    for filename in filenames:
+        output_shapefile_left = f'CollocationsRight.shp'  # Add an underscore for better naming
+        test = pd.read_csv(os.path.join('/Users/wndrsn', (filename + '.csv')),
+                        usecols=columnsList)
+        test = test.drop_duplicates(subset=['filename_left'], keep='first', inplace=False, ignore_index=False)
+        test = test.drop_duplicates(subset=['filename_right'], keep='first', inplace=False, ignore_index=False)
+        
+        # Process the left side
+        geometry_left = [Point(xy) for xy in zip(test['Long0_right'], test['Lat0_right'])]
+        gdf_left2 = gpd.GeoDataFrame(test, geometry=geometry_left, crs='EPSG:4326')
 
-for filename in filenames:
-    output_shapefile_left = f'ATL_{filename}.shp'  # Add an underscore for better naming
-    test = pd.read_csv(os.path.join('/Users/wndrsn', (filename + '.csv')),
-                       usecols=['filename_right', 'filename_left', 'Long0_left', 'Lat0_left','Time0_left'])
-    test = test.drop_duplicates(subset=['filename_left'], keep='first', inplace=False, ignore_index=False)
-    test = test.drop_duplicates(subset=['filename_right'], keep='first', inplace=False, ignore_index=False)
-    
-    # Process the left side
-    geometry_left = [Point(xy) for xy in zip(test['Long0_left'], test['Lat0_left'])]
-    gdf_left2 = gpd.GeoDataFrame(test, geometry=geometry_left, crs='EPSG:4326')
+        # Append gdf_left2 to the list
+        gdf_list.append(gdf_left2)
+        print(f'Finished processing {filename}!')
 
-    # Append gdf_left2 to the list
-    gdf_list.append(gdf_left2)
+    # Concatenate all GeoDataFrames in the list
+    gdf_left = gpd.GeoDataFrame(pd.concat(gdf_list, ignore_index=True), crs='EPSG:4326')
+    df_left = pd.concat(gdf_list,ignore_index = True)
+    df_left.to_csv(f'Colocations_list_{side}.csv')
+    # Save the final GeoDataFrame to a shapefile
+    gdf_left.to_file(output_shapefile_left, mode='w', driver='ESRI Shapefile')
 
-# Concatenate all GeoDataFrames in the list
-gdf_left = gpd.GeoDataFrame(pd.concat(gdf_list, ignore_index=True), crs='EPSG:4326')
-df_left = pd.concat(gdf_list,ignore_index = True)
-df_left.to_csv('Colocations_list.csv')
-# Save the final GeoDataFrame to a shapefile
-gdf_left.to_file(output_shapefile_left, mode='w', driver='ESRI Shapefile')
+if __name__ == "__main__":
+    create_shapefile(['filename_left','filename_right', 'Long0_right', 'Lat0_right','Time0_right'],'Left')
+    create_shapefile(['filename_left','filename_right', 'Long0_left', 'Lat0_left','Time0_left'],'Right')
+
+
